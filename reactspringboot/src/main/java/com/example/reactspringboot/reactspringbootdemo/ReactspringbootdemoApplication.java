16d4007d45c1b9588f0bba2159d0b6b1d5ece38f
kerathy <xxyxuxiaoyan@126.com>
2021-04-16 19:48:10 -0700

    react springboot commit1

---
+++ reactspringboot/src/main/java/com/example/reactspringboot/reactspringbootdemo/ReactspringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactspringboot.reactspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactspringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactspringbootdemoApplication.class, args);
+    }
+
+}
---
+++ reactspringboot/src/test/java/com/example/reactspringboot/reactspringbootdemo/ReactspringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactspringboot.reactspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactspringbootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
